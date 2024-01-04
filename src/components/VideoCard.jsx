59b1d2ea554f454aabf7c0c1eeebdc7aa322c93a
Nandiniranga <127603735+Nandiniranga@users.noreply.github.com>
2024-01-04 12:10:57 +0530

    Added VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,16 +2,35 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import { Typography, Card , CardContent , CardMedia } 
 from '@mui/material';
-import CheckCircleOutlineIcon
-from '@mui/icons-material/CheckCircleOutline';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import { demoThumbnailUrl , demoVideoUrl , demoVideoTitle , demoChannelUrl , demoChannelTitle } from '../utils/constants';
 
 const VideoCard = ({video : {
-  id : { videoId }, snippet } }) => {
+  id : { videoId , channelId }, snippet } }) => {
   return (
-    <div>
-    VideoCard
-    </div>
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+        image = {snippet?.thumbnails?.high?.url}
+        alt = {snippet?.title}
+        sx = {{width : 358 , height : 180}}
+        />
+        <CardContent sx = {{backgroundColor : '#1e1e1e', 
+        height : '106px'}}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Typography varaint = "subtitle1" fontWeight= "bold" color = "#FFF">
+          {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+        </Typography>
+        </Link>
+        <Link to={channelId  ? `/video/${channelId}` : demoVideoUrl}>
+        <Typography varaint = "subtitle2" fontWeight= "bold" color = "gray">
+        {snippet?.channelTitle || demoChannelTitle}
+        <CheckCircleOutlineIcon  sx={{ fontSize: "16px", color: "gray", ml: "5px" }}/>
+        </Typography>
+        </Link>
+        </CardContent>
+      </Link>
+    </Card>
   )
 }
 
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,7 +4,6 @@ const options = {
     url: BASE_URL,
     params: {
       maxResults: '50',
-      order: 'date'
     },
     headers: {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
